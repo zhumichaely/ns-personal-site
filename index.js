85c7f3b37e80a0bbaf19982e31fa8e7311003616
zhumichaely <57302029+zhumichaely@users.noreply.github.com>
2020-06-19 20:56:47 -0400

    changes html served based on whether user has visited

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const session = require("express-session");
+const crypto = require("crypto");
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(
+  session({
+    secret: crypto.randomBytes(64).toString("hex"),
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+ if (req.session.visited) {
+    res.sendFile(`${__dirname}/index-revisit.html`);
+ }
+ else {
+    req.session.visited = true;
+    res.sendFile(`${__dirname}/index.html`);
+ }
+});
+
+app.listen(port);
